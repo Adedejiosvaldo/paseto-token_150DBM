005c906589f05af7dd7277fdc42ce074d4df9fd6
Joseph <adedejiosvaldo@gmail.com>
2024-07-23 19:11:21 +0100

    Feature -> Add course controller

---
+++ src/api/courses/course.controller.ts
@@ -0,0 +1,9 @@
+const GetCourses = async (req: Request, res: Response) => {};
+
+const GetACourse = async (req: Request, res: Response) => {};
+
+const CreateACourse = async (req: Request, res: Response) => {};
+
+const UpdateACourse = async (req: Request, res: Response) => {};
+
+const DeleteACourse = async (req: Request, res: Response) => {};
